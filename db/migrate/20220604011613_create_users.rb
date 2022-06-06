ec173fbfa2c61d0f5752712cb933dc5f54f8971c
Takahiro <deaman6948@gmail.com>
2022-06-06 14:10:32 +0000

    Finish modeling-user

--- db/migrate/20220604011613_create_users.rb
+++ db/migrate/20220604011613_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    create_table :users do |t| #　tの中にname emailのカラムが入っている
       t.string :name
       t.string :email
 
