74ba1947add09135709027d39e1d11ac44b6049c
Takahiro <deaman6948@gmail.com>
2022-06-17 02:27:04 +0000

    7.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_tolen
   before_save {self.email = email.downcase}
   
    validates :name, presence: true, length:{maximum:50}
@@ -23,5 +24,10 @@ class User < ApplicationRecord
 
    def User.new_token
      SecureRandom.urlsafe_base64
+   end
+   
+   def remember
+     self.remember_token = User.new_token
+     update_attribute(:remember_digest,User.digest(remember_token))
    end
 end
