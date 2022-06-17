cb3605e0c9783a699e38bc729da979997a19fe7e
Takahiro <deaman6948@gmail.com>
2022-06-17 05:12:55 +0000

    7.1.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,9 +10,16 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   
   def log_out
-    session.delete(:user.id)
+    forget(current_user)
+    session.delete(:user_id)
     @current_user = nil
   end  
    
