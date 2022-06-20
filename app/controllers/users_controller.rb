9f471f27a419bc262165889bd849ea26bf7b2a1a
Takahiro <deaman6948@gmail.com>
2022-06-20 14:49:41 +0000

    8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,62 @@
 class UsersController < ApplicationController
-  before_action :set_user, only:[:show,:edit,:update]
-  before_action :logged_in_user, only: [:show,:edit,:update]
-  before_action :correct_user, only: [:edit,:update]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
-   
   end
 
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success]="新規作成に成功しました"
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
-      #保存に成功した場合は、ここに記述した処理が実行される。
     else
-    render:new
+      render :new
     end
   end
-  
+
   def edit
-   
   end
-  
+
   def update
-   
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
-      redirect_to @user  
+      redirect_to @user
     else
-      render:edit
+      render :edit      
     end
   end
-  
-  
+
   private
-   def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-   end
-   
-   def set_user
-     @user = User.find(params[:id])
-   end
-   
-  def logged_in_user
-     unless logged_in?
-     　flash[:danger] = "ログインしてください。"
-     　redirect_to login_url
-     end
-  end
-  
-  def correct_user
-   redirect_to(root_url) unless current_user?(@user)
-  end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
